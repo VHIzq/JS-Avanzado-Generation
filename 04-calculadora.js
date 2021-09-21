047f40107dcf52a0c230739adfb0b3d8bc433fc1
Victor Izquierdo <vhmizq@gmail.com>
2021-09-21 18:02:49 -0500

    ejercicios JS

--- 04-calculadora.js
+++ 04-calculadora.js
@@ -8,15 +8,21 @@ document.write(tuEdad); */
 
 /* function edadFunc(edad){
   edad = parseInt(prompt("En qué año naciste?"));
-  const edadCalculada = 2021 - edad;
+  const edadCalculada = 2021 - edad
   const imprimirEdad = `<h1>Tu edad es ${edadCalculada} años</h1>`;
   document.write(imprimirEdad);
 }
 edadFunc();
  */
+
+
+
+
+
+
 /* Indicar si un ´´numero es par o impar */
 
-function numero(num){
+/* function numero(num){
   num = parseInt(prompt("Ingresa un número"));
   if(num % 2 == 0){
     document.write(`<h1>El número ${num} es par</h1>`);
@@ -24,4 +30,27 @@ function numero(num){
     document.write(`<h1>El número ${num} es impar</h1>`);
   }
 }
-numero();
+numero(); */
+
+
+
+/* Números pares entre dos números */
+
+function imparNum(){
+  num1 = parseInt(prompt("Introduzca el primero número"));
+  num2 = parseInt(prompt("Introduzca el segundo número"));
+  let minNum = Math.min(num1, num2);
+  let maxNum = Math.max(num1, num2);
+  for(let i = minNum + 1; i < maxNum; i++){
+    if(i % 2 != 0){
+      let impar = `<h1>${i} </h1>`;
+      document.write(impar);
+    }
+  }
+}
+imparNum();
+
+
+
+
+
