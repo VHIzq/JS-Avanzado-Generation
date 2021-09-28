c6469d929cdaec85d634a0f6ba1b6ed7a9f67885
Victor Izquierdo <vhmizq@gmail.com>
2021-09-28 18:00:32 -0500

    ejerci¿¿cicio-api-perros

--- 17-ejercicio-api.js
+++ 17-ejercicio-api.js
@@ -3,20 +3,21 @@ const $contenedor = document.querySelector("#contenedor");
 let content = "";
 
 let mostrar_cartas = () => {
-  fetch("https://jsonplaceholder.typicode.com/albums/1/photos%22")
+  fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
     .then((resp) => resp.json())
     .then((data) => {
       let count = Object.keys(data).length;
       for (let i = 0; i < count; i++) {
-        content +=  <div id="tarjeta" class="card m-3" style="width: 18rem;">
-                        <img src="${data[n].url}" class="card-img-top" alt="..." />
+        content += `
+            <div id="tarjeta" class="card m-3" style="width: 18rem;">
+                        <img src="${data[i].url}" class="card-img-top" alt="..." />
                         <div class="card-body">
                             <p class="card-text">${data[i].title}</p>
                         </div>
                     </div>;
+                  `
       }
       $contenedor.innerHTML = content;
     });
 };
-
 mostrar_cartas();
